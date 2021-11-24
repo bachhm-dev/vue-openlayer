523403cf28c87b8f6629be24a29a5dc61439203b
bach <bach@offspringdigital.com>
2021-11-24 12:17:04 +0700

    Test

--- deploy-stand-alone-app.sh
+++ deploy-stand-alone-app.sh
@@ -4,6 +4,6 @@
 #   && cp .buildkite/secrets/credentials ~/.aws/credentials
 
 mkdir for_upload \
-  && aws s3 cp s3://bachhm-build-artifacts/${1}/${FOLDER} for_upload --recursive \
+  && aws s3 cp s3://bachhm-build-artifacts/ for_upload --recursive \
   && aws s3 cp for_upload s3://test-bachhm --recursive \
-  && aws s3 rm s3://bachhm-build-artifacts/${1}/${FOLDER} --recursive
+  && aws s3 rm s3://bachhm-build-artifacts/ --recursive
